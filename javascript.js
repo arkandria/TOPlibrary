7a2e1b6d7bccc32ff62a933f1348c189fbb5411b
arkandria <miguelfariasgp@gmail.com>
2022-08-15 16:24:49 -0500

    add HTM elements and show hide form

---
+++ javascript.js
@@ -0,0 +1,42 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+Book.prototype.reportBook = function (){
+    let isRead = this.read===true ? "already read" : "not read yet";
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${isRead}`;
+}
+
+const theHobbit = new Book ("The Hobbit", "J.R.R. Tolkien", 295, true);
+
+console.log(theHobbit.reportBook());
+myLibrary.push(theHobbit);
+
+
+
+
+function addBookToLibrary() {
+    let bookCard = document.createElement('div');
+    let bookTitle = document.createElement('div');
+    let bookDescription = document.createElement('div');
+
+    div.appendChild(div)
+
+
+    let temp = new Book (title, author, pages, read)
+    myLibrary.push(temp)
+
+
+  }
+
+const container = document.querySelector('#books-container');
+const form = document.querySelector('form')
+
+const formCaller = () => {
+    form.style.display = 'grid';
+}
